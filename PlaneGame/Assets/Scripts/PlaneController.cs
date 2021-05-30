cc8c50674f3a7dd599e1bbcbab357aa39d990c1c
Sameer Khan <samsam23122001@gmail.com>
2021-05-30 16:10:40 -0400

    Setup Reference Objects and Test Plane

---
+++ PlaneGame/Assets/Scripts/PlaneController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneController : MonoBehaviour
+{
+    // Parameters
+    public float planeSpeed = 10f;
+
+    void Awake(){
+        return;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        if (Mathf.Abs (h) > 0.1f || Mathf.Abs (v) > 0.1f) {
+			transform.Translate (new Vector3 (planeSpeed * v * Time.deltaTime, 0f, planeSpeed * h * Time.deltaTime));
+		}
+    }
+}
