d4db2dba866174a7925fc6cc3d8190d472f44f34
Sameer Khan <samsam23122001@gmail.com>
2021-06-02 00:02:14 -0400

    Updated gitignore and added plane movement
    Added a standard gitingore to avoid a million files changing. Also added some basic plane movement that needs further tuning for our liking.

--- PlaneGame/Assets/Scripts/PlaneController.cs
+++ PlaneGame/Assets/Scripts/PlaneController.cs
@@ -5,20 +5,54 @@ using UnityEngine;
 public class PlaneController : MonoBehaviour
 {
     // Parameters
-    public float planeSpeed = 10f;
+    //public float planeSpeed = 10f;
+    public float forwardSpeed = 25f, strafeSpeed = 7.5f, hoverSpeed = 5f;
+    private float activeForwardSpeed, activeStrafeSpeed, activeHoverSpeed;
+    private float forwardAcceleration = 2.5f, strafeAcceleration = 2f, hoverAcceleration = 2f;
 
-    void Awake(){
-        return;
+    public float lookRateSpeed = 90f;
+    private Vector2 lookInput, screenCenter, mouseDistance;
+
+    private float rollInput;
+    public float rollSpeed = 90f, rollAcceleration = 3.5f;
+
+    // Start is called before the first frame update
+    void Start(){
+        screenCenter.x = Screen.width * .5f;
+        screenCenter.y = Screen.height * .5f;
+
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = false;
     }
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+        lookInput.x = Input.mousePosition.x;
+        lookInput.y = Input.mousePosition.y;
 
+        mouseDistance.x = (lookInput.x - screenCenter.x) / screenCenter.y;
+        mouseDistance.y = (lookInput.y - screenCenter.y) / screenCenter.y;
+
+        mouseDistance = Vector2.ClampMagnitude(mouseDistance, 1f);
+
+        rollInput = Mathf.Lerp(rollInput, Input.GetAxisRaw("Roll"), rollAcceleration * Time.deltaTime);
+
+        transform.Rotate(-mouseDistance.y * lookRateSpeed * Time.deltaTime, mouseDistance.x * lookRateSpeed * Time.deltaTime, rollInput * rollSpeed * Time.deltaTime, Space.Self);
+
+        activeForwardSpeed = Mathf.Lerp(activeForwardSpeed, Input.GetAxisRaw("Vertical") * forwardSpeed, forwardAcceleration * Time.deltaTime);
+        activeStrafeSpeed = Mathf.Lerp(activeStrafeSpeed, Input.GetAxisRaw("Horizontal") * strafeSpeed, strafeAcceleration * Time.deltaTime);
+        activeHoverSpeed = Mathf.Lerp(activeHoverSpeed, Input.GetAxisRaw("Altitude") * hoverSpeed, hoverAcceleration * Time.deltaTime);
+
+        transform.position += transform.forward * activeForwardSpeed * Time.deltaTime;
+        transform.position += (transform.right * activeStrafeSpeed * Time.deltaTime) + (transform.up * activeHoverSpeed * Time.deltaTime);
+
+
+
+        /*
         if (Mathf.Abs (h) > 0.1f || Mathf.Abs (v) > 0.1f) {
             //multiply by "-h" to correct horizontal direction
 			transform.Translate (new Vector3 (planeSpeed * v * Time.deltaTime, 0f, planeSpeed * -h * Time.deltaTime));
 		}
+        */
     }
 }
