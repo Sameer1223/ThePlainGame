994fa941f522c634d5e460d109603bda5974e847
Sameer Khan <samsam23122001@gmail.com>
2021-06-16 00:24:34 -0400

    Completed basic plane movement
    Plane movement with speed adjustment, roll, rudder, and mouse pitch movement on 1 axis

--- PlaneGame/Assets/Scripts/PlaneController.cs
+++ PlaneGame/Assets/Scripts/PlaneController.cs
@@ -4,55 +4,55 @@ using UnityEngine;
 
 public class PlaneController : MonoBehaviour
 {
-    // Parameters
-    //public float planeSpeed = 10f;
-    public float forwardSpeed = 25f, strafeSpeed = 7.5f, hoverSpeed = 5f;
-    private float activeForwardSpeed, activeStrafeSpeed, activeHoverSpeed;
-    private float forwardAcceleration = 2.5f, strafeAcceleration = 2f, hoverAcceleration = 2f;
+    //Constants
+    public const int minSpeed = 10, maxSpeed = 30;
+    public const int speedChange = 10;
+
+    //Attributes
+    private float activeForwardSpeed = 20f;
+    private float forwardAcceleration = 2.5f;
+
+    private float rollInput;
+    private float rollSpeed = 90f, rollAcceleration = 3.5f;
+
+    private float rudderInput;
+    private float rudderSpeed = 90f, rudderAcceleration = 3.5f;
 
     public float lookRateSpeed = 90f;
     private Vector2 lookInput, screenCenter, mouseDistance;
 
-    private float rollInput;
-    public float rollSpeed = 90f, rollAcceleration = 3.5f;
-
     // Start is called before the first frame update
     void Start(){
-        screenCenter.x = Screen.width * .5f;
+        //screenCenter.x = Screen.width * .5f;
         screenCenter.y = Screen.height * .5f;
 
         Cursor.lockState = CursorLockMode.Confined;
         Cursor.visible = false;
     }
+
     // Update is called once per frame
     void Update()
     {
-        lookInput.x = Input.mousePosition.x;
+        // Get mouse location
+        //lookInput.x = Input.mousePosition.x;
         lookInput.y = Input.mousePosition.y;
 
-        mouseDistance.x = (lookInput.x - screenCenter.x) / screenCenter.y;
+        // Get distance from cursor to centre of screen
+        //mouseDistance.x = (lookInput.x - screenCenter.x) / screenCenter.y;
         mouseDistance.y = (lookInput.y - screenCenter.y) / screenCenter.y;
 
         mouseDistance = Vector2.ClampMagnitude(mouseDistance, 1f);
 
         rollInput = Mathf.Lerp(rollInput, Input.GetAxisRaw("Roll"), rollAcceleration * Time.deltaTime);
+        rudderInput = Mathf.Lerp(rudderInput, Input.GetAxisRaw("Horizontal"), rudderAcceleration * Time.deltaTime);
 
-        transform.Rotate(-mouseDistance.y * lookRateSpeed * Time.deltaTime, mouseDistance.x * lookRateSpeed * Time.deltaTime, rollInput * rollSpeed * Time.deltaTime, Space.Self);
+        transform.Rotate(-mouseDistance.y * lookRateSpeed * Time.deltaTime, rudderInput * rudderSpeed * Time.deltaTime, rollInput * rollSpeed * Time.deltaTime, Space.Self);
+        
+        activeForwardSpeed = Mathf.Lerp(activeForwardSpeed, Mathf.Clamp(activeForwardSpeed + 
+        Input.GetAxisRaw("Vertical") * speedChange, minSpeed, maxSpeed), forwardAcceleration * Time.deltaTime);
 
-        activeForwardSpeed = Mathf.Lerp(activeForwardSpeed, Input.GetAxisRaw("Vertical") * forwardSpeed, forwardAcceleration * Time.deltaTime);
-        activeStrafeSpeed = Mathf.Lerp(activeStrafeSpeed, Input.GetAxisRaw("Horizontal") * strafeSpeed, strafeAcceleration * Time.deltaTime);
-        activeHoverSpeed = Mathf.Lerp(activeHoverSpeed, Input.GetAxisRaw("Altitude") * hoverSpeed, hoverAcceleration * Time.deltaTime);
-
+        Debug.Log(activeForwardSpeed);
         transform.position += transform.forward * activeForwardSpeed * Time.deltaTime;
-        transform.position += (transform.right * activeStrafeSpeed * Time.deltaTime) + (transform.up * activeHoverSpeed * Time.deltaTime);
-
-
-
-        /*
-        if (Mathf.Abs (h) > 0.1f || Mathf.Abs (v) > 0.1f) {
-            //multiply by "-h" to correct horizontal direction
-			transform.Translate (new Vector3 (planeSpeed * v * Time.deltaTime, 0f, planeSpeed * -h * Time.deltaTime));
-		}
-        */
     }
 }
+
