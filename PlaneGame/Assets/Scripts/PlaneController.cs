554fb8f340ceeec53541f075bdc446882cd617bc
Sameer Khan <samsam23122001@gmail.com>
2021-06-19 16:20:23 -0400

    Completed plane movement
    Plane movement is complete

--- PlaneGame/Assets/Scripts/PlaneController.cs
+++ PlaneGame/Assets/Scripts/PlaneController.cs
@@ -4,26 +4,36 @@ using UnityEngine;
 
 public class PlaneController : MonoBehaviour
 {
+    public GameObject plane;
+
     //Constants
     public const int minSpeed = 10, maxSpeed = 30;
     public const int speedChange = 10;
 
-    //Attributes
+    //[Attributes]
+    // Speed and acceleration values
     private float activeForwardSpeed = 20f;
     private float forwardAcceleration = 2.5f;
 
+    // Rotation inputs and speed
     private float rollInput;
     private float rollSpeed = 90f, rollAcceleration = 3.5f;
 
     private float rudderInput;
-    private float rudderSpeed = 90f, rudderAcceleration = 3.5f;
+    private float rudderSpeed = 30f, rudderAcceleration = 1.5f;
 
-    public float lookRateSpeed = 90f;
+    // Mouse control
+    public float lookRateSpeed = 45f;
     private Vector2 lookInput, screenCenter, mouseDistance;
+
+    // Rotation values
+    private float pitch, roll;
+
+    // Conditions
+    private bool allowRudder;
 
     // Start is called before the first frame update
     void Start(){
-        //screenCenter.x = Screen.width * .5f;
         screenCenter.y = Screen.height * .5f;
 
         Cursor.lockState = CursorLockMode.Confined;
@@ -34,24 +44,29 @@ public class PlaneController : MonoBehaviour
     void Update()
     {
         // Get mouse location
-        //lookInput.x = Input.mousePosition.x;
         lookInput.y = Input.mousePosition.y;
 
         // Get distance from cursor to centre of screen
-        //mouseDistance.x = (lookInput.x - screenCenter.x) / screenCenter.y;
         mouseDistance.y = (lookInput.y - screenCenter.y) / screenCenter.y;
-
         mouseDistance = Vector2.ClampMagnitude(mouseDistance, 1f);
 
+        // Get plane rotation
+        pitch = plane.transform.localRotation.eulerAngles.x;
+        roll = plane.transform.localRotation.eulerAngles.z;
+
+        // Rotation inputs
         rollInput = Mathf.Lerp(rollInput, Input.GetAxisRaw("Roll"), rollAcceleration * Time.deltaTime);
-        rudderInput = Mathf.Lerp(rudderInput, Input.GetAxisRaw("Horizontal"), rudderAcceleration * Time.deltaTime);
+        bool allowRudder = (Mathf.Abs(pitch) < 20 || Mathf.Abs(pitch) > 340) && (Mathf.Abs(roll) < 10 || Mathf.Abs(roll) > 350);
+        //Debug.Log("Allow Rudder: " + allowRudder + " Pitch: " + pitch + " Roll: " +  roll);
+        rudderInput = Mathf.Lerp(rudderInput, allowRudder ? Input.GetAxisRaw("Horizontal") : 0, rudderAcceleration * Time.deltaTime);
+
 
         transform.Rotate(-mouseDistance.y * lookRateSpeed * Time.deltaTime, rudderInput * rudderSpeed * Time.deltaTime, rollInput * rollSpeed * Time.deltaTime, Space.Self);
         
         activeForwardSpeed = Mathf.Lerp(activeForwardSpeed, Mathf.Clamp(activeForwardSpeed + 
         Input.GetAxisRaw("Vertical") * speedChange, minSpeed, maxSpeed), forwardAcceleration * Time.deltaTime);
 
-        Debug.Log(activeForwardSpeed);
+        //Debug.Log(activeForwardSpeed);
         transform.position += transform.forward * activeForwardSpeed * Time.deltaTime;
     }
 }
