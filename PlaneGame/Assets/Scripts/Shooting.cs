3389f25fa59a6fa37b2bdc2e0aea5f3d3d562a05
Sameer Khan <samsam23122001@gmail.com>
2021-06-27 15:33:28 -0400

    Made important changes to shooting

--- PlaneGame/Assets/Scripts/Shooting.cs
+++ PlaneGame/Assets/Scripts/Shooting.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlaneShooting : MonoBehaviour
+public class Shooting : MonoBehaviour
 {
     public float cooldownSpeed;
     public float fireRate;
